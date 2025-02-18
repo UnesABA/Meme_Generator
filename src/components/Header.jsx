2ed60f0f042a657e8fcd508c51f6e76fc3a8e4a8
UnesABA <unesaitbenali@gmail.com>
2025-02-18 09:36:21 +0100

    finished header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../index.css'
+import trollFace from '/Troll_Face.png'
+
+const Header = () => {
+  return (
+    <>
+      <div className= "header">
+        <div>
+          <img src= {trollFace} className= "troll-face" alt="Troll Face" />
+        </div>
+        <div>
+          <h1 className= "title">Meme Generator</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header
